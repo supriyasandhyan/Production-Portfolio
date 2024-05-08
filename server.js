7c9b811769ccc37c38e09a9853632289f7b23b12
Supriya Premraj Sandhyan <47947363+supriyasandhyan@users.noreply.github.com>
2024-05-08 23:33:48 +0530

    My Portfolio on production
    its going live - with responsive code

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const path = require('path')
+
+//dotenv configuration
+dotenv.config()
+
+//rest object
+const app = express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+//static files
+app.use(express.static(path.join(__dirname, './portfolio/build')))
+
+//routes
+app.use('/api/v1/portfolio', require("./routes/portfolioRoutes"));
+
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname, './portfolio/build/index.html'))
+})
+
+//port
+const PORT = process.env.PORT || 8080
+
+//listen
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`);
+})
